77eafa7401437efdba5a46a295eb249a1e7040c2
Wilker <wilkermizael@gmail.com>
2023-06-25 18:35:24 -0300

    teste 1.0

--- src/app.js
+++ src/app.js
@@ -8,11 +8,7 @@ app.use(express.json())
 let meuAvatar=''
 const login=[];
 const tweets=[
-	{
-		username: "bobesponja",
-		avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-		tweet: "Eu amo hambúrguer de siri!"
-	}
+	
 ]
 
 
@@ -34,11 +30,11 @@ app.get('/tweets', (req,res) =>{
 })
 
 app.post('/tweets', (req,res) =>{
-    
+    const {username, tweet} = req.body
     if(!login){
         return res.status(401).send('UNAUTHORIZED')
     }
-    const {username, tweet} = req.body
+    
     const dados= {
         username,
         tweet,
