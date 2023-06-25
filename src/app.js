ccbd7f62b5c3597be1006314ae13e36771c125a8
Wilker <wilkermizael@gmail.com>
2023-06-25 17:20:40 -0300

    feito todos os get e post

--- src/app.js
+++ src/app.js
@@ -5,18 +5,49 @@ import cors from "cors"
 const app = express()
 app.use(cors())
 app.use(express.json())
-const login=[]
+let meuAvatar=''
+let contador =0;
+const publi =[]
+const login=[];
+const tweets=[
+	{
+		username: "bobesponja",
+		avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+		tweet: "Eu amo hambúrguer de siri!"
+	}
+]
+
+
 app.post('/sign-up', (req,res) =>{
     const {username, avatar} = req.body;
+    const loginPost={username,avatar};
     if(!username || !avatar){
         return res.status(401).send('O envio dos dados são obrigatórios')
     }
-    loginPost={username,avatar};
     login.push(loginPost)
+    meuAvatar = login[0].avatar
     res.status(200).send('Ok')
 })
 
 app.get('/tweets', (req,res) =>{
     
+    tweets.push(publi)
+    res.status(200).send(tweets)
+    
+})
+
+app.post('/tweets', (req,res) =>{
+    
+    if(!login){
+        return res.status(401).send('UNAUTHORIZED')
+    }
+    const {username, tweet} = req.body
+    const dados= {
+        username,
+        tweet,
+        avatar:meuAvatar
+        }
+    tweets.push(dados)
+    res.send("ok")
 })
 app.listen(5000)
