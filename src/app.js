57392b7e31b48104028f51dffd3420905c98414c
Wilker <wilkermizael@gmail.com>
2023-06-26 14:20:08 -0300

    teste 1.2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 import express from "express"
 import cors from "cors"
 
-
 const app = express()
 app.use(cors())
 app.use(express.json())
 let meuAvatar=''
-const login=[]
-const tweets=[
+let login=[]
+let tweets=[
 	
 ]
 
@@ -18,6 +17,7 @@ app.post('/sign-up', (req,res) =>{
     /*if(!username || !avatar){
         return res.status(401).send('O envio dos dados são obrigatórios')
     }*/
+    
     login.push(loginPost)
     meuAvatar = login[0].avatar
     res.status(200).send('Ok')
@@ -25,25 +25,43 @@ app.post('/sign-up', (req,res) =>{
 
 app.get('/tweets', (req,res) =>{
     
+
     res.status(200).send(tweets)
-    
+
 })
 
 app.post('/tweets', (req,res) =>{
+    
+   
     const {username, tweet} = req.body
     let nome = username
-    console.log(nome)
     if(!nome){
         return res.status(401).send('UNAUTHORIZED')
     }
     
-    const dados= {
+    const dados ={
         username,
         tweet,
         avatar:meuAvatar
-        }
-    tweets.push(dados)
-    //console.log(dados.length)
-    res.send("ok")
+        };
+  
+    
+    
+    if(tweets.length < 11){
+        tweets.splice(-tweets.length,0,dados)
+       
+    }
+
+    if(tweets.length > 10){
+        
+        const corte = tweets.pop()
+        
+        
+    }
+    res.send('ok')
+
+    
+    
+    
 })
 app.listen(5000)
